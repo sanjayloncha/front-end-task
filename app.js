91620550eec10ae5bdec449ff188993c88aaa952
sanjay121531 <71871677+sanjayloncha@users.noreply.github.com>
2023-02-27 22:27:16 +0530

    react added with html

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let App = ()=>{
+    return (
+        <h1>This is app component.</h1>
+    )
+}
+
+
+let root = ReactDOM.createRoot(document.getElementById("root")) ;
+    root.render(<App />) ;
