89df4ef337b853a8b948e432dc7a80973f73c08c
sanjay121531 <71871677+sanjayloncha@users.noreply.github.com>
2023-02-28 01:55:13 +0530

    data managed in table format

--- app.js
+++ app.js
@@ -1,11 +1,110 @@
-'use strict';
+"use strict";
 
-let App = ()=>{
-    return (
-        <h1>This is app component.</h1>
-    )
+const { useState } = React;
+
+function TableData() {
+  const [data, setData] = useState([
+    [
+      "Technical Audit",
+      "Website Access",
+      "Google Search Console Access",
+    ],
+    [
+      "Onboardng Call",
+      "Google Analytics Access",
+      "On Page Optimization",
+    ],
+    [
+      "Competitor Analysis",
+      "Premium Press Release",
+      "Site Level Optimization",
+    ]
+  ]);
+
+  const [month, setMonth] = useState([
+    {
+      MONTH: "MONTH 1",
+    },
+    {
+      MONTH: "MONTH 2",
+    },
+    {
+      MONTH: "MONTH 3",
+    },
+  ]);
+
+
+  function handleEdit(index, column, value) {
+    const newRows = [...state];
+    newRows[index][column] = value;
+    setState(newRows);
+  }
+
+  return (
+    <>
+      <div className="container">
+        <div className="table_heading">
+          {month.map((item) => {
+            return (
+              <div >
+                <h2>{item.MONTH}</h2>
+              </div>
+            );
+          })}
+        </div>
+        <div className="table_row">
+        {data.map((elem,index) => {
+            return (
+                <div key={index} className="table_row_data">
+              {
+                  elem.map((item,index)=>{
+                    return (
+                        <p key={index} >{item}</p>
+                    )
+                })
+              }
+            </div>
+          );
+        })}
+        </div>
+      </div>
+    </>
+  );
 }
 
+function EditableCell({ value, onEdit }) {
+  const [editing, setEditing] = useState(false);
+  const [inputValue, setInputValue] = useState(value);
 
-let root = ReactDOM.createRoot(document.getElementById("root")) ;
-    root.render(<App />) ;
+  function handleEdit() {
+    setEditing(true);
+  }
+
+  function handleSave() {
+    setEditing(false);
+    onEdit(inputValue);
+  }
+
+  function handleChange(event) {
+    setInputValue(event.target.value);
+  }
+
+  return (
+    <td>
+      {editing ? (
+        <>
+          <input type="text" value={inputValue} onChange={handleChange} />
+          <button onClick={handleSave}>Save</button>
+        </>
+      ) : (
+        <>
+          {value}
+          <button onClick={handleEdit}>Edit</button>
+        </>
+      )}
+    </td>
+  );
+}
+
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<TableData />);
